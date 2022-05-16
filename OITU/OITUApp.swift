ddbf854bfaec8d3a6bb638149c69d6e7cfa020f2
ianwash <ianwash@live.unc.edu>
2022-05-16 11:52:04 -0400

    Initial Commit

---
+++ OITU/OITUApp.swift
@@ -0,0 +1,17 @@
+//
+//  OITUApp.swift
+//  OITU
+//
+//  Created by Ian Washabaugh on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct OITUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
